5d1c54d67855eac8af0a802cb0a732332ca0ac70
武继飞 <wujifei@thundersdata.com>
2019-12-14 16:43:37 +0800

    测试

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+
+        log.info("测试");
+
     }
 
 }
